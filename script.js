97cb65fdf18e9490c97d17cc43023c91fd336027
Aexki <50047387+Aexki@users.noreply.github.com>
2020-09-18 19:30:24 +0530

    updated

--- script.js
+++ script.js
@@ -17,8 +17,13 @@ function choice() {
     document.getElementById("choice").style.display = "block"
 }
 
-function game() {
+function gamechoose() {
     document.getElementById("choice").style.display = "none"
+    document.getElementById("gamechoose").style.display = "block"
+}
+
+function game() {
+    document.getElementById("gamechoose").style.display = "none"
     document.getElementById("game").style.display = "block"
 }
 
@@ -26,6 +31,8 @@ function go() {
     document.getElementById("front").style.display = "none"
     document.getElementById("choice").style.display = "none"
     document.getElementById("game").style.display = "none"
+    document.getElementById("truth").style.display = "none"
+    document.getElementById("dare").style.display = "none"
     document.getElementById("photobackground").style.display = "block"
     setTimeout(() => {
         document.getElementById("wish").play()
@@ -38,6 +45,8 @@ function contact() {
     document.getElementById("photobackground").style.display = "none"
     document.getElementById("choice").style.display = "none"
     document.getElementById("game").style.display = "none"
+    document.getElementById("truth").style.display = "none"
+    document.getElementById("dare").style.display = "none"
     document.getElementById("contact").style.display = "block"
     $("#check").prop("checked", false)
 }
@@ -60,13 +69,14 @@ function sendEmail() {
                         To: 'autoaexki@gmail.com',
                         From: "learningaexki@gmail.com",
                         Subject: "Message from HBDS",
-                        Body: "Name: " + name + " " +
-                            "Email: " + email + " " +
-                            "Number: " + number + " " +
-                            "Message: " + message,
+                        Body: `Name: ` + name + `<br>
+                               Email: ` + email + `<br>
+                               Number: ` + number + `<br>
+                               Message: ` + message,
                     })
                     .then(function(message) {
-                        window.alert("Your Message has been Successfully Recorded..")
+                        window.alert("Your Message has been successfully recorded..")
+                        history.go(0)
                     });
             } else {
                 window.alert("The Mobile No. must be at least 10 digit long!");
@@ -80,4 +90,54 @@ function sendEmail() {
         window.alert("Fill the fields to submit..!!");
         return
     }
+}
+
+questions = ['Tell me about your first kiss.', 'Tell me about your most awkward date.', 'What is the naughtiest thing you’ve done in public?', 'What do most people think is true about you, but isn’t?', 'What is something your friends would never expect that you do?', 'What is the most embarrassing thing in your room?']
+dares = ['Dance with no music for 1 minute and record it.', 'Draw a mustache on your face and click a picture of it.', 'Sing a song(No lip synching) and record it.']
+var questionasked;
+
+
+function truth() {
+    document.getElementById("game").style.display = "none"
+    document.getElementById("truth").style.display = "block"
+    questionasked = questions[Math.floor(Math.random() * 6)]
+    document.getElementById("question").innerHTML = "<h4>" + questionasked + "</h4>"
+}
+
+function checkanswer() {
+    if (document.getElementById("truthanswer").value == '') {
+        window.alert("Answer something")
+    } else {
+        Email.send({
+            Host: "smtp.gmail.com",
+            Username: "learningaexki@gmail.com",
+            Password: "aexki123",
+            To: 'autoaexki@gmail.com',
+            From: "learningaexki@gmail.com",
+            Subject: "Truth Answer",
+            Body: `Question: ` + questionasked + `<br>
+                   Answer: ` + document.getElementById("truthanswer").value,
+        });
+        go()
+    }
+}
+
+function dare() {
+    document.getElementById('game').style.display = "none"
+    document.getElementById('dare').style.display = "block"
+    questionasked = dares[Math.floor(Math.random() * 3)]
+    document.getElementById("task").innerHTML = "<h4>" + questionasked + "</h4>"
+}
+
+function daredone() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "learningaexki@gmail.com",
+        Password: "aexki123",
+        To: 'autoaexki@gmail.com',
+        From: "learningaexki@gmail.com",
+        Subject: "Dare Task",
+        Body: `Dare: ` + questionasked,
+    });
+    go()
 }
